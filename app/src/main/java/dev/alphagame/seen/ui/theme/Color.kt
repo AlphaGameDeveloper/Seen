f8a0a3e1db7ea0f8a1ed3f60c4f30507dac12ad7
amanomg8 <amanomg8@gmail.com>
2025-08-30 23:50:03 -0700

    Incorporated our base colors into the app.
    Dark mode: Blue, Purple, Purple-Grey.
    
    Light Mode: Orange, Grey, Black.

--- app/src/main/java/dev/alphagame/seen/ui/theme/Color.kt
+++ app/src/main/java/dev/alphagame/seen/ui/theme/Color.kt
@@ -4,7 +4,14 @@ import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
+val Blue = Color(0xFF94CBFF)
 val Pink80 = Color(0xFFEFB8C8)
+
+val LightGrey = Color(0xFFF0F0F0) //Background
+val Orange = Color(0xFFFF7300) //Primary
+val Grey = Color(0xFF7D7D7D) //Secondary
+val Black = Color(0xFF000000) //Tertiary
+
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
--- app/src/main/java/dev/alphagame/seen/ui/theme/Theme.kt
+++ app/src/main/java/dev/alphagame/seen/ui/theme/Theme.kt
@@ -12,18 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue,
+    secondary = Purple80,
+    tertiary = PurpleGrey80,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Orange,
+    secondary = Grey,
+    tertiary = Black,
+    background = LightGrey
 
     /* Other default colors to override
-    background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
@@ -37,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
 fun SeenTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
