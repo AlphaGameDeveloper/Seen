a00db205d9cbe2efdc17937dca6fd8ff3fcf459d
amanomg8 <amanomg8@gmail.com>
2025-09-02 15:40:11 -0700

    New light mode color scheme
    orange-blue-beige

--- app/src/main/java/dev/alphagame/seen/ui/theme/Color.kt
+++ app/src/main/java/dev/alphagame/seen/ui/theme/Color.kt
@@ -7,10 +7,14 @@ val PurpleGrey80 = Color(0xFFCCC2DC)
 val Blue = Color(0xFF94C0FF)
 val Pink80 = Color(0xFFEFB8C8)
 
-val LightGrey = Color(0xFFF0F0F0) //Background
-val Orange = Color(0xFFFF7300) //Primary
-val Grey = Color(0xFF7D7D7D) //Secondary
-val Black = Color(0xFF000000) //Tertiary
+val LightGrey = Color(0xFFF3F4F6) //Background
+
+
+
+val Orange = Color(0xFFF97316)  //Pri1
+val Grey = Color(0xFFd6cfc4)  //Ter1
+val Blue2 = Color(0xFF5ca4a9) //Sec1
+val Black = Color(0xFF000000)
 
 
 val Purple40 = Color(0xFF6650a4)
--- app/src/main/java/dev/alphagame/seen/ui/theme/Theme.kt
+++ app/src/main/java/dev/alphagame/seen/ui/theme/Theme.kt
@@ -19,25 +19,16 @@ private val DarkColorScheme = darkColorScheme(
 
 private val LightColorScheme = lightColorScheme(
     primary = Orange,
-    secondary = Grey,
-    tertiary = Black,
+    secondary = Blue2,
+    tertiary = Grey,
+
     background = LightGrey,
     surface = LightGrey
-
-    /* Other default colors to override
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun SeenTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
@@ -46,11 +37,9 @@ fun SeenTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
