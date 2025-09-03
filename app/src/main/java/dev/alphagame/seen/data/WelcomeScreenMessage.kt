56c67d9bff57fb40e71b66a700495621c197b0cd
amanomg8 <amanomg8@gmail.com>
2025-09-02 20:08:12 -0700

    shortened the messages

--- app/src/main/java/dev/alphagame/seen/data/WelcomeScreenMessage.kt
+++ app/src/main/java/dev/alphagame/seen/data/WelcomeScreenMessage.kt
@@ -7,9 +7,9 @@ class WelcomeScreenMessage {
     companion object {
         private val messages: Array<String> = arrayOf(
             "Take a moment to check in with yourself",
-            "It's ok to not be ok, it's not ok to stay that way",
-            "'There is hope, even when your brain tells you there isn't' - John Green",
-            "'You are not alone. You are seen. I am with you. You are not alone.' - Shonda Rhimes",
+            "It's ok to not be ok",
+            "There is hope, even when your brain tells you there isn't",
+            "You are not alone. You are seen.",
             "Welcome back!",
             "Good to see you again!",
             "Let's begin something great!",
@@ -17,7 +17,6 @@ class WelcomeScreenMessage {
             "Hope you're having a good day!",
             "Let's make today count!"
         )
-
         fun getRandomWelcomeScreenMessage(): String {
             // damien - java.util.Random.nextInt is between 0 (inclusive) and
             // X (exclusive), so we don't need to subtract 1 from size because
