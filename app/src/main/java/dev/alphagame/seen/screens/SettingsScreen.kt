7c29c2e49128767c291f043b4a7410567a48ec8e
Damien Boisvert <damien@alphagame.dev>
2025-09-27 20:23:32 -0700

    refactor: Add FeatureFlags.SETTINGS_STATUS_DOTS

--- app/src/main/java/dev/alphagame/seen/screens/SettingsScreen.kt
+++ app/src/main/java/dev/alphagame/seen/screens/SettingsScreen.kt
@@ -143,7 +143,9 @@ fun SettingsScreen(
                 }
             },
             actions = {
-                HealthStatusDots(healthStatusManager = healthStatusManager)
+                if (FeatureFlags.SETTINGS_STATUS_DOTS) {
+                    HealthStatusDots(healthStatusManager = healthStatusManager)
+                }
             },
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = MaterialTheme.colorScheme.surface,
