8e4b37d31012723a05285adda867257673206688
amanomg8 <amanomg8@gmail.com>
2025-10-05 23:33:35 -0700

    Changed the "loading ai analysis" UI to have a grey background instead of secondary (looks better than the orange on blue we had prior)

--- app/src/main/java/dev/alphagame/seen/screens/results/BasicResultsScreen.kt
+++ app/src/main/java/dev/alphagame/seen/screens/results/BasicResultsScreen.kt
@@ -1,17 +1,17 @@
 // Seen - Mental Health Application
 //     Copyright (C) 2025  Damien Boisvert
 //                   2025  Alexander Cameron
-// 
+//
 //     Seen is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
 //     the Free Software Foundation, either version 3 of the License, or
 //     (at your option) any later version.
-// 
+//
 //     Seen is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //     GNU General Public License for more details.
-// 
+//
 //     You should have received a copy of the GNU General Public License
 //     along with Seen.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -110,7 +110,7 @@ fun BasicResultsScreen(
             modifier = Modifier.fillMaxWidth(),
             colors = CardDefaults.cardColors(
                 containerColor = when (aiAnalysisState) {
-                    is AIAnalysisState.Loading -> MaterialTheme.colorScheme.primaryContainer
+                    is AIAnalysisState.Loading -> Color(0xFFE6E6E6)
                     is AIAnalysisState.Success -> MaterialTheme.colorScheme.tertiaryContainer
                     is AIAnalysisState.Error -> MaterialTheme.colorScheme.errorContainer
                     else -> MaterialTheme.colorScheme.surfaceVariant
